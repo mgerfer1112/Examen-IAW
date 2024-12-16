34903f574b70372222a41a660d24cf28973346bd
mgerfer1112 <m.gervilla1112@gmail.com>
2024-12-16 12:30:18 +0000

    a

--- Scripts/nfs-client.sh
+++ Scripts/nfs-client.sh
@@ -8,7 +8,7 @@ sudo mount $SERVER_IP:$MOODLE_DIRECTORY $MOODLE_DIRECTORY
 df -h
 
 sudo mount $SERVER_IP:$MOODLE_DATA_DIRECTORY $MOODLE_DATA_DIRECTORY
-
+df -h
 
  
 echo "$SERVER_IP:$MOODLE_DIRECTORY $MOODLE_DIRECTORY nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0" >> /etc/fstab
