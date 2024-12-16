c9e2c08eab6d8a8af1067f32d23cdacf1bf22228
mgerfer1112 <m.gervilla1112@gmail.com>
2024-12-16 12:21:31 +0000

    NFS

--- Scripts/nfs-server.sh
+++ Scripts/nfs-server.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+
 set -ex
+
 source .env
+
 apt update
+
 apt install nfs-kernel-server -y
-mkdir -p $WORDPRESS_DIRECTORY
-sudo chown nobody:nogroup $WORDPRESS_DIRECTORY
+
+mkdir -p $MOODLE_DIRECTORY
+mkdir -p $MOODLE_DATA_DIRECTORY
+
+sudo chown nobody:nogroup $MOODLE_DIRECTORY
+sudo chown nobody:nogroup $MOODLE_DATA_DIRECTORY
 cp ../exports /etc/exports
 
-sed -i "s#CLIENT_IP_1#$Network#" /etc/exports
-sed -i "s#WORDPRESS_DIRECTORY#$WORDPRESS_DIRECTORY#" /etc/exports
+sed -i "s#Network#$Network#" /etc/exports
+sed -i "s#MOODLE_DIRECTORY#$MOODLE_DIRECTORY#" /etc/exports
+sed -i "s#MOODLE_DATA_DIRECTORY#$MOODLE_DATA_DIRECTORY#" /etc/exports
 
 systemctl restart nfs-kernel-server
