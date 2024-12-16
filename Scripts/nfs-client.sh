367018076784e075fb7684744f5a45a6191f6fd3
mgerfer1112 <m.gervilla1112@gmail.com>
2024-12-16 12:28:38 +0000

    cambios

--- Scripts/nfs-client.sh
+++ Scripts/nfs-client.sh
@@ -4,8 +4,12 @@ source .env
 apt update
 apt install nfs-common -y
 
-sudo mount $SERVER_IP:$WORDPRESS_DIRECTORY $WORDPRESS_DIRECTORY
+sudo mount $SERVER_IP:$MOODLE_DIRECTORY $MOODLE_DIRECTORY
 df -h
 
+sudo mount $SERVER_IP:$MOODLE_DATA_DIRECTORY $MOODLE_DATA_DIRECTORY
+
+
  
-echo "$SERVER_IP:$WORDPRESS_DIRECTORY $WORDPRESS_DIRECTORY nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0" >> /etc/fstab
+echo "$SERVER_IP:$MOODLE_DIRECTORY $MOODLE_DIRECTORY nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0" >> /etc/fstab
+echo "$SERVER_IP:$MOODLE_DATA_DIRECTORY $MOODLE_DATA_DIRECTORY nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0" >> /etc/fstab
