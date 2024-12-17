fb4f6bef83d187b2dca8e75e7d75ec37e4c1c4a9
mgerfer1112 <m.gervilla1112@gmail.com>
2024-12-17 15:25:55 +0000

    fin

--- Scripts/deploy_moodle_frontend.sh
+++ Scripts/deploy_moodle_frontend.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 
-
 set -ex
 
 # Importamos el archivo de variables
 source .env
 
-
 # Eliminamos descargas previas de Moodle en /tmp
 rm -rf /tmp/moodle-latest-405.tgz*
 
 # # Descargamos la última versión estable de Moodle
-
 wget https://download.moodle.org/download.php/direct/stable405/moodle-latest-405.tgz -P /tmp
 
 # Extraemos el archivo descargado
 tar -xzf /tmp/moodle-latest-405.tgz -C /tmp
 
-
 # Movemos los archivos extraídos al directorio de instalación de Moodle
 mv /tmp/moodle/* "$MOODLE_DIRECTORY"
 
-# Configuramos el directorio de datos de Moodle
+# Configuramos el directorio de datos de Moodle y sus permisos.
 chown -R www-data:www-data "$MOODLE_DATA_DIRECTORY"
 chmod -R 770 "$MOODLE_DATA_DIRECTORY"
 
@@ -32,14 +28,13 @@ chmod -R 755 "$MOODLE_DIRECTORY"
 # Copiamos el archivo .htaccess para configurar el acceso y seguridad en el servidor web
 cp ../htaccess/.htaccess /var/www/html/.htaccess
 
-
 # Copiamos el archivo de configuración de Apache
 cp ../conf/000-default.conf /etc/apache2/sites-available/000-default.conf
-
 
 # Instalamos las extensiones PHP requeridas para Moodle
 sudo apt install -y php-curl php-zip php-xml php-mbstring php-gd php-intl php-imagick php-soap php-xmlrpc php-mysqli php-bz2 php-opcache
 
+#Reiniciamos apache.
 systemctl restart apache2
 
 
@@ -54,11 +49,6 @@ sudo sed -i 's/^;max_execution_time\s=.*/max_execution_time = 300/' /etc/php/8.3
 sudo sed -i 's/^;memory_limit\s=.*/memory_limit = 256M/' /etc/php/8.3/cli/php.ini
 sudo sed -i 's/^;post_max_size\s=.*/post_max_size = 50M/' /etc/php/8.3/cli/php.ini
 sudo sed -i 's/^;upload_max_filesize\s=.*/upload_max_filesize = 50M/' /etc/php/8.3/cli/php.ini
-
-#Cambiamos el máximo de caracteres para que cumpla los requisitos de moodle
-sudo sed -i 's/^;max_input_vars = 1000/max_input_vars = 5000/' /etc/php/8.3/cli/php.ini
-sudo sed -i 's/^;max_input_vars = 1000/max_input_vars = 5000/' /etc/php/8.3/apache2/php.ini
-
 
 sudo -u www-data php "$MOODLE_DIRECTORY/admin/cli/install.php" \
   --wwwroot="$MOODLE_URL" \
@@ -84,7 +74,11 @@ sudo -u www-data php "$MOODLE_DIRECTORY/admin/cli/install.php" \
 sudo sed -i "s|^;date.timezone =|date.timezone = UTC|" /etc/php/*/cli/php.ini
 sudo sed -i "s|^;date.timezone =|date.timezone = UTC|" /etc/php/*/apache2/php.ini
 
-#CGF
+#Cambiamos el máximo de caracteres para que cumpla los requisitos de moodle
+sudo sed -i 's/^;max_input_vars = 1000/max_input_vars = 5000/' /etc/php/8.3/cli/php.ini
+sudo sed -i 's/^;max_input_vars = 1000/max_input_vars = 5000/' /etc/php/8.3/apache2/php.ini
+
+#Cambiamos las variables para habilitar el proxy inverso.
 sed -i "/\$CFG->admin/a \$CFG->reverseproxy=1;\n\$CFG->sslproxy=1;" $MOODLE_DIRECTORY/config.php
 
 # Reiniciamos Apache
--- Scripts/nfs-server.sh
+++ Scripts/nfs-server.sh
@@ -2,21 +2,30 @@
 
 set -ex
 
+#Importación de variables.
 source .env
 
+#Actualización del sistema.
 apt update
 
+#Instalación del servidor NFS.
 apt install nfs-kernel-server -y
 
+#Creamos las carpetas que alojará el servidor
 mkdir -p $MOODLE_DIRECTORY
 mkdir -p $MOODLE_DATA_DIRECTORY
 
+#Cambiamos los permisos a nobody:nogroup.
 sudo chown nobody:nogroup $MOODLE_DIRECTORY
 sudo chown nobody:nogroup $MOODLE_DATA_DIRECTORY
+
+#Copiamos nuestro archivo exports al sistema.
 cp ../exports /etc/exports
 
+#Sustituimos con sed las siguientes palabras por nuestras variables.
 sed -i "s#Network#$Network#" /etc/exports
 sed -i "s#MOODLE_DIRECTORY#$MOODLE_DIRECTORY#" /etc/exports
 sed -i "s#MOODLE_DATA_DIRECTORY#$MOODLE_DATA_DIRECTORY#" /etc/exports
 
+#Reiniciamos el servidor NFS.
 systemctl restart nfs-kernel-server
